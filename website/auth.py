42a553374ddd31f7fb7814435e3c619f300297dc
Eromosele Israel <romszikey@gmail.com>
2025-08-15 12:40:54 +0100

    Add files via upload

--- website/auth.py
+++ website/auth.py
@@ -55,7 +55,7 @@ def Sign_up():
             new_user = User(email=email, first_name=firstName,password=generate_password_hash(password1))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created.', category='success')
             return redirect(url_for('views.home'))
     return render_template("sign_up.html", user=current_user)
